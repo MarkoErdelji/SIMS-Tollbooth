8ce290ea8f8b62bcbef002f677743e6e769bbdf9
prlina01 <36077702+prlina01@users.noreply.github.com>
2022-06-24 18:52:01 -0700

    feat: Add pricelist, ticket, transaction and a vehicle model.

---
+++ Simsprojekat/Model/Ticket.cs
@@ -0,0 +1,39 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simsprojekat.Model
+{
+    [BsonIgnoreExtraElements]
+
+    public class Ticket
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? _Id { get; set; }
+
+        [BsonElement("id")]
+        public int Id { get; set; }
+
+        [BsonElement("entryTime")]
+        public string EntryTime { get; set; }
+
+        [BsonElement("vehicle")]
+        public Vehicle Vehicle { get; set; }
+
+        [BsonElement("entryStationId")]
+
+        public int EntryStationId { get; set; }
+
+        [BsonElement("done")]
+
+        public bool Done { get; set; }
+
+
+
+    }
+}
