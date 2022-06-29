93a491584387cd3691aa838e485320647e26fbdf
MarkoErdelji <erdeljimarko@gmail.com>
2022-06-29 19:50:58 +0200

    feat: Added observer to station manager form

--- Simsprojekat/Model/Device.cs
+++ Simsprojekat/Model/Device.cs
@@ -5,11 +5,12 @@ using System.Text;
 using System.Threading.Tasks;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
+using Simsprojekat.Observer;
 
 namespace Simsprojekat.Model
 {
     [BsonIgnoreExtraElements]
-    public class Device
+    public class Device : IObservable
     {
         [BsonElement("name")]
         public string Name { get; set; }
@@ -17,5 +18,20 @@ namespace Simsprojekat.Model
         [BsonElement("faulty")]
         public bool Faulty { get; set; }
 
+        private List<IObserver> _observers = new List<IObserver>();
+
+        public void Attach(IObserver observer)
+        {
+            if (_observers == null) _observers = new List<IObserver>();
+            _observers.Add(observer);
+        }
+
+        public void Notify()
+        {
+            _observers.ForEach(o =>
+            {
+                o.Update(this);
+            });
+        }
     }
 }
